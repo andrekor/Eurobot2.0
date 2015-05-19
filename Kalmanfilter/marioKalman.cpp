c4740e55a43a2846cc8a9910021f2c94852c5094
André Kramer Orten <andrekor@ifi.uio.no>
2015-05-19 20:17:55 +0200

    tweaking on kalman parameters

--- Kalmanfilter/marioKalman.cpp
+++ Kalmanfilter/marioKalman.cpp
@@ -50,7 +50,7 @@ void marioKalman::initKalman() {
 	float p[N][N] = {{1,0,0},{0,1,0},{0,0,1}}; //Identity matric. Will be updated at later point
 	float h[N][N] = {{1,0,0},{0,1,0},{0,0,0}}; //Identity matric. (no rotation) 
 	float q[N][N] = {{0.9, 0, 0}, {0, 0.9, 0}, {0, 0, 1}};
-	float r[N][N] = {{0.75, 0, 0}, {0, 0.85, 0}, {0, 0, 1}};//A lot of error for the beacons, should be handled as noise
+	float r[N][N] = {{0.5, 0, 0}, {0, 0.6, 0}, {0, 0, 0}};//A lot of error for the beacons, should be handled as noise
 	//std::cout << "a: " << sizeof(a) << " float: " << sizeof(float) << " a[1] " << sizeof(a[1]) << std::endl;
 	for (int i = 0; i < (sizeof(a[1])/sizeof(float)); i++) {
 		for (int j = 0; j < (sizeof(a[1])/sizeof(float)); j++) {
